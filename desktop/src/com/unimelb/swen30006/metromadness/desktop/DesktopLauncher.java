e4bc21294b9dbe307dde59c116da780b50e69b8d
tinbao <tinb@student.unimelb.edu.au>
2017-04-28 00:07:57 +1000

    removed files

--- desktop/src/com/unimelb/swen30006/metromadness/desktop/DesktopLauncher.java
+++ desktop/src/com/unimelb/swen30006/metromadness/desktop/DesktopLauncher.java
@@ -1,8 +1,6 @@
 package com.unimelb.swen30006.metromadness.desktop;
 
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
@@ -11,12 +9,6 @@ import com.unimelb.swen30006.metromadness.MetroMadness;
 public class DesktopLauncher {
 	public static void main (String[] arg) throws FileNotFoundException {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1960;
-		config.height = 1080;
 		new LwjglApplication(new MetroMadness(), config);
-		/*
-		PrintStream out = new PrintStream(new FileOutputStream("output.txt"));
-		System.setOut(out);
-		*/
 	}
 }
